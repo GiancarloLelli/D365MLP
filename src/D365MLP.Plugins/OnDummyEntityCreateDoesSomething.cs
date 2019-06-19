564f20d18f306c710ae23963acda21eec37fba34
Giancarlo Lelli <gcarlo.lelli@live.com>
2019-06-19 10:54:23 +0200

    initial commit

---
+++ src/D365MLP.Plugins/OnDummyEntityCreateDoesSomething.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace D365MLP.Plugins
+{
+    public class OnDummyEntityCreateDoesSomething : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/D365MLP.WebResources/mlp_/scripts/D365MLP.mlp_dummyentity.js
@@ -0,0 +1,32 @@
+﻿if (typeof (D365MLP) == "undefined") { D365MLP = { __namespace: true }; }
+if (typeof (D365MLP.mlp_dummyentity) == "undefined") { D365MLP.mlp_dummyentity = { __namespace: true }; }
+
+D365MLP.mlp_dummyentity.Form = new function () {
+    var _self = this;
+
+    _self.OnLoad = function () {
+        var ai = window.appInsights;
+        ai.trackEvent({ name: 'some event' });
+        ai.trackPageView({ name: 'some page' });
+        ai.trackPageViewPerformance({ name: 'some page', url: 'some url' });
+        ai.trackException({ exception: new Error('some error') });
+        ai.trackTrace({ message: 'some trace' });
+        ai.trackMetric({ name: 'some metric', average: 42 });
+        ai.trackDependencyData({ absoluteUrl: 'some url', responseCode: 200, method: 'GET', id: 'some id' });
+        ai.startTrackPage("pageName");
+        ai.stopTrackPage("pageName", { customProp1: "some value" });
+        ai.startTrackEvent("event");
+        ai.stopTrackEvent("event", { customProp1: "some value" });
+        ai.flush();
+
+        var telemetryInitializer = (envelope) => {
+            envelope.data.someField = 'This item passed through my telemetry initializer';
+        };
+
+        ai.addTelemetryInitializer(telemetryInitializer);
+        ai.trackTrace({ message: 'This message will use a telemetry initializer' });
+
+        ai.addTelemetryInitializer(() => false); // Nothing is sent after this is executed
+        ai.trackTrace({ message: 'this message will not be sent' }); // Not sent
+    }
+}

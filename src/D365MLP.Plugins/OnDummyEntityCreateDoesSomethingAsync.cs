b122218a2787e7b6e08e0b275f82bc90fa17785d
Giancarlo Lelli <gcarlo.lelli@live.com>
2019-06-19 20:00:43 +0200

    Added ApplicationInsights SDK

--- src/D365MLP.Plugins/OnDummyEntityCreateDoesSomethingAsync.cs
+++ src/D365MLP.Plugins/OnDummyEntityCreateDoesSomethingAsync.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Xrm.Sdk;
 using System;
@@ -10,20 +11,17 @@ namespace D365MLP.Plugins
         public void Execute(IServiceProvider serviceProvider)
         {
             var context = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
+            var trace = serviceProvider.GetService(typeof(ITracingService)) as ITracingService;
             var factory = serviceProvider.GetService(typeof(IOrganizationServiceFactory)) as IOrganizationServiceFactory;
             var service = factory.CreateOrganizationService(context.InitiatingUserId);
+
             var dummyEntity = context.InputParameters.ContainsKey("Target") ? context.InputParameters["Target"] as Entity : null;
 
-            if (dummyEntity != null)
-                ExecuteLogic(dummyEntity, service, context.InitiatingUserId, context.RequestId.GetValueOrDefault(Guid.Empty));
-        }
+            var appInsights = new TelemetryClient(new TelemetryConfiguration("38765d3d-9ba0-4e85-aafe-e832f40cf44e"));
+            appInsights.Context.User.Id = context.InitiatingUserId.ToString();
+            appInsights.Context.Operation.Id = context.RequestId.GetValueOrDefault(Guid.Empty).ToString();
 
-        private void ExecuteLogic(Entity dummyEntity, IOrganizationService service, Guid userId, Guid reqId)
-        {
-            var ai = new TelemetryClient(new TelemetryConfiguration("38765d3d-9ba0-4e85-aafe-e832f40cf44e"));
-            ai.Context.User.AuthenticatedUserId = userId.ToString();
-            ai.Context.Operation.Id = reqId.ToString();
-            ai.TrackTrace($"Record Created with Id={dummyEntity.Id}");
+            appInsights.TrackTrace(new TraceTelemetry($"Record Created with Id={dummyEntity.Id}", SeverityLevel.Information));
         }
     }
 }
